b3e8170c2d6436d6f1fadfefb10db5e2cf522a17
zaidkamil <xaidmetamorphos@gmail.com>
2024-01-14 12:56:44 +0530

    added a database code

--- code/app.py
+++ code/app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template,redirect
+from flask import Flask, render_template,redirect,request
+from database import User, add_to_db, open_db
+
 app = Flask(__name__)
 
 
@@ -8,10 +10,23 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        print("Email =>", email)
+        print("Password =>", password)
+        # logic
     return render_template('login.html')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        email = request.form.get('email')
+        password = request.form.get('password')
+        cpassword = request.form.get('cpassword')
+        print(username, email, password, cpassword)
+        # logic
     return render_template('register.html')
 
 if __name__ == '__main__':
---
+++ code/database.py
@@ -0,0 +1,45 @@
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import engine
+from sqlalchemy import Column, Integer, String, DateTime
+from datetime import datetime
+from sqlalchemy.orm import sessionmaker
+
+# base model class
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(50), nullable=False)
+    email = Column(String(64), unique=True)
+    password = Column(String(64))
+    created_at = Column(DateTime, default=datetime.now)
+
+    def __str__(self):
+        return self.username
+
+# more classes for other tables
+    
+# utility functions
+def open_db():
+    engine = engine.create_engine('sqlite:///project.db', echo=True)
+    session =  sessionmaker(bind=engine)
+    return session()
+
+def add_to_db(object):
+    db = open_db()
+    db.add(object)
+    db.commit()
+
+if __name__ == "__main__":
+    # create engine
+    # mysql settings
+    # database_name = 'projectdb'
+    # host = 'localhost'
+    # username = 'root'
+    # password = 'root'
+    # port = 3306
+    # engine = engine.create_engine(f'mysql+pymysql://{username}:{password}@{host}:{port}/{database_name}')
+    # demo
+    engine = engine.create_engine('sqlite:///project.db', echo=True)
+    Base.metadata.create_all(engine)
